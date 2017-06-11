c1a3c6d92a6347e78db14a9d3d8e3abfcd417257
gmpatil <gmpatil.nb@gmail.com>
2017-06-11 13:01:49 -0700

    T2 ukf wip

--- term2/p07_ukf/src/tools.cpp
+++ term2/p07_ukf/src/tools.cpp
@@ -12,7 +12,49 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
-  */
+    */
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+  
+	// estimation vector size should not be zero
+  if (estimations.size() <= 0) {
+      cout << " estimations is NULL or empty" << endl;
+      return rmse;
+  }
+
+  if ( ground_truth.size() <= 0) {
+      cout << " ground_truth is NULL or empty" << endl;
+      return rmse;
+  }
+        
+	// estimation vector size should equal ground truth vector size
+  if ( (estimations.size() != ground_truth.size())) {
+      cout << " estimations.size() != ground_truth.size() " << endl;
+      return rmse;
+  }
+        
+  VectorXd sqRes(4);
+  sqRes << 0.0, 0.0, 0.0, 0.0 ;
+  VectorXd sqRest(4);
+        
+	// accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i){
+    //cout << estimations.at(i) << endl;
+    //cout << ground_truth.at(i) << endl;
+    sqRest = ( estimations.at(i) - ground_truth.at(i)) ;
+    //cout << sqRest << endl << endl;
+    sqRest = sqRest.array() * sqRest.array();
+    //cout << sqRest << endl << endl;
+    sqRes = sqRes + sqRest;
+    //cout << sqRes << endl << endl;
+	}
+
+	// calculate the mean
+  sqRes /= estimations.size();
+        
+	// calculate the squared root
+  rmse = sqRes.array().sqrt();
+  
+  return rmse;
 }
--- term2/p07_ukf/src/ukf.cpp
+++ term2/p07_ukf/src/ukf.cpp
@@ -46,10 +46,7 @@ UKF::UKF() {
   std_radrd_ = 0.3;
 
   /**
-  TODO:
-
   Complete the initialization. See ukf.h for other member properties.
-
   Hint: one or more values initialized above might be wildly off...
   */
   is_initialized_ = false;
@@ -78,13 +75,9 @@ UKF::~UKF() {}
  */
 void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   /**
-  TODO:
-
   Complete this function! Make sure you switch between lidar and radar
   measurements.
   */
-  
-  long long previous_timestamp_ = 0.0;
   
   if (!is_initialized_) {
     // first measurement
@@ -103,8 +96,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
       x_(2) = 0.0;
       x_(3) = 0.0;
       x_(4) = 0.0;
-    }
-    else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
       x_(0) = meas_package.raw_measurements_(0);
       x_(1) = meas_package.raw_measurements_(1);
       x_(2) = 0.0;
@@ -112,17 +104,22 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
       x_(3) = 0.0;
     }
 
-    previous_timestamp_ = meas_package.timestamp_;
-    
+    time_us_ = meas_package.timestamp_;
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
   }
 
+  long long previous_timestamp_ = time_us_;
   //dt - expressed in seconds
-	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	
-	previous_timestamp_ = measurement_pack.timestamp_;
+	float dt = (meas_package.timestamp_ - previous_timestamp_) / 1000000.0;	
+	time_us_ = meas_package.timestamp_;
 
+  if (meas_package.sensor_type_ == MeasurementPackage::RADAR) {
+    
+  } else if (meas_package.sensor_type_ == MeasurementPackage::LASER) {
+    
+  }
   
 }
 
@@ -168,4 +165,97 @@ void UKF::UpdateRadar(MeasurementPackage meas_package) {
 
   You'll also need to calculate the radar NIS.
   */
+}
+
+// Private methods.
+void generateSigmaPoints(MatrixXd* Xsig_out, VectorXd x, int n_x,
+        MatrixXd P) {
+  
+  //define spreading parameter
+  double lambda = 3 - n_x;
+
+  //create sigma point matrix
+  MatrixXd Xsig = MatrixXd(n_x, 2 * n_x + 1);
+
+  //calculate square root of P
+  MatrixXd A = P.llt().matrixL();
+  
+  //calculate sigma points ...
+  //set sigma points as columns of matrix Xsig
+  // 1st column mean or X
+  Xsig.col(0)  = x;
+  
+  MatrixXd nxSigmasDisc = (sqrt((lambda + n_x)) * A);
+  //MatrixXd nxSigmas = x + nxSigmasDisc;
+  //         nxSigmas = x - nxSigmasDisc;
+  for (int j=1; j<= n_x; j++) {
+    for (int i=0; i< x.size(); i++) {
+      Xsig(i,j) = x(i) + nxSigmasDisc(i, j-1);
+      Xsig(i,j + n_x) = x(i) - nxSigmasDisc(i, j-1);      
+    }
+  }
+
+  //print result
+  // std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+  //write result
+  *Xsig_out = Xsig;
+}
+
+void generateAugmentedSigmaPoints(MatrixXd* Xsig_out, VectorXd x, int n_x, MatrixXd P ) {
+  //set augmented dimension
+  int n_aug = 7;
+
+  //Process noise standard deviation longitudinal acceleration in m/s^2
+  double std_a = 0.2;
+
+  //Process noise standard deviation yaw acceleration in rad/s^2
+  double std_yawdd = 0.2;
+
+  //define spreading parameter
+  double lambda = 3 - n_aug;
+
+  //create augmented mean vector
+  VectorXd x_aug = VectorXd(7);
+
+  //create augmented state covariance
+  MatrixXd P_aug = MatrixXd(7, 7);
+
+  //create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);
+ 
+  //create augmented mean state
+  x_aug.head(n_x) = x;
+  x_aug(n_x) = 0; //mean a not std_a;
+  x_aug(n_x + 1) = 0; //mean yaw not std_yawdd;
+  
+  //create augmented covariance matrix
+  P_aug.topLeftCorner(n_x, n_x) = P;
+  MatrixXd Q = MatrixXd(2, 2);
+  Q(0,0) = std_a * std_a;
+  Q(0,1) = 0 ;
+  Q(1,0) = 0 ;
+  Q(1,1) = std_yawdd * std_yawdd ;
+  P_aug.bottomRightCorner(2,2) = Q;
+  
+  //create square root matrix
+  MatrixXd A = P_aug.llt().matrixL();
+  
+  //create augmented sigma points
+  //set first column of sigma point matrix
+  Xsig_aug.col(0)  = x_aug;  
+
+  //set remaining sigma points
+  for (int i = 0; i < n_aug ; i++)
+  {
+    Xsig_aug.col(i+1)     = x_aug + sqrt(lambda+n_aug) * A.col(i);
+    Xsig_aug.col(i+1+n_aug) = x_aug - sqrt(lambda+n_aug) * A.col(i);
+  }  
+
+  //print result
+  std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+
+  //write result
+  *Xsig_out = Xsig_aug;
+  
 }
