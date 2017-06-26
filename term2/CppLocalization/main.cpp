c38892cd5c78c3cc111b33d29c55a3f5e2942db2
gmpatil <gmpatil.nb@gmail.com>
2017-06-26 01:53:22 -0700

    Localization

---
+++ term2/CppLocalization/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: girish
+ *
+ * Created on June 25, 2017, 6:17 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int mainAAA(int argc, char** argv) {
+
+  return 0;
+}
+
