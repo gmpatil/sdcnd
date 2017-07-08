9138657da70718a94b40457a4facaecd6abae6f9
gmpatil <gmpatil.nb@gmail.com>
2017-07-08 16:12:16 -0700

    t2-p3/p8 - Complete. Tested with 50 and 500 particles.

--- term2/p08_kv/src/particle_filter.cpp
+++ term2/p08_kv/src/particle_filter.cpp
@@ -25,7 +25,10 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	//   x, y, theta and their uncertainties from GPS) and all weights to 1.
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-  num_particles = 50;
+  
+  // 500particles  passed (error x .110, y .103, yaw .004)
+  // 50particles  passed (error x .120, y .116, yaw .004)
+  num_particles = 500;  
   double std_x = std[0];
   double std_y = std[1];
   double std_theta = std[2];
